b0192ea43ae32cb1ad41a3377964eaed2c5050ea
Lloml <474823637@qq.com>
2021-09-16 19:01:59 +0800

    first commit

---
+++ src/main/java/cn/lloml/destinyrecruit/DestinyRecruitApplication.java
@@ -0,0 +1,13 @@
+package cn.lloml.destinyrecruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinyRecruitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DestinyRecruitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lloml/destinyrecruit/DestinyRecruitApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lloml.destinyrecruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinyRecruitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
